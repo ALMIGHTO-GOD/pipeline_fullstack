24ab2de3d96950635bff36ac959705fb77056875
ALMIGHTO-GOD <80496539+ALMIGHTO-GOD@users.noreply.github.com>
2026-05-04 18:06:58 -0700

    pruebas test

---
+++ test.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const options = {
+  hostname: "localhost",
+  port: 3000,
+  path: "/api/products",
+  method: "GET",
+};
+
+const req = http.request(options, (res) => {
+  if (res.statusCode === 403 || res.statusCode === 401) {
+    console.log("Test OK (La API protegio la ruta correctamente)");
+    process.exit(0);
+  } else {
+    console.error("Test fallo");
+    process.exit(1);
+  }
+});
+
+req.on("error", () => {
+  console.error("Error conexion");
+  process.exit(1);
+});
+
+req.end();
